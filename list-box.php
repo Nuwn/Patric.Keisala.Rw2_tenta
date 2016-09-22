651b13d67d3c6fc4cfeda037b5d1fb9aeea0a289
Patric keisala <finne_no1@hotmail.com>
2016-09-22 16:29:27 +0200

    louis

--- list-box.php
+++
@@ -1,7 +0,0 @@
-<!-- components/atoms/list-box.php -->
-<li class="list__league"> 
-  <p>league box</p>
-</li>
-<li class="game"> 
-  <p>game box </p>
-</li>
--- startAtomic.bat
+++
@@ -1,2 +0,0 @@
-START cmd.exe /k "cd c:\xampp\htdocs\atomic-docs-master\atomic-core & gulp"
-start "" http://127.0.0.1/atomic-docs-master/atomic-core
